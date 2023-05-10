b6361c7a7e04fd53389635ee3762fa559601c351
Sulthan Azka <114067296+Narwhalssss@users.noreply.github.com>
2023-05-10 16:42:43 +0700

    Update itemSearchBTree.java

--- itemSearchBTree.java
+++ itemSearchBTree.java
@@ -1,3 +1,18 @@
+public class itemSearchBTree {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        int nodeCount = tree.countNodes();
+        System.out.println("Number of nodes in the binary tree: " + nodeCount);
+    }
+}
+
 class Node {
     int data;
     Node left;
@@ -26,21 +41,5 @@ class BinaryTree {
     }
     public int countNodes() {
         return countNodes(root);
-    }
-}
-
-
-public class itemSearchBTree {
-    public static void main(String[] args) {
-        BinaryTree tree = new BinaryTree();
-
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.right = new Node(5);
-
-        int nodeCount = tree.countNodes();
-        System.out.println("Number of nodes in the binary tree: " + nodeCount);
     }
 }
